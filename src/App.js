c16e68e0b311474eeaaaadc77f7eb7967e08e47c
Jesus Herrera <jesusherrera1594@gmail.com>
2020-08-14 20:16:40 -0600

    styled de contenedores

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import Header from './componets/Header';
+import styled from '@emotion/styled';
 
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin:0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background: #fff;
+  padding: 3rem;
+`
 
 function App() {
   return (
-    <div className="App">
+    <Contenedor>
      <Header
         titulo='Cotizador de Seguros'
      />
-    </div>
+     <ContenedorFormulario>
+       
+     </ContenedorFormulario>
+    </Contenedor>
   );
 }
 
